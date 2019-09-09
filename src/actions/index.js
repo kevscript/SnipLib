08dc4dbade2b7f9ac53a82c20be18482fbf169a9
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-09-09 16:37:38 +0200

    created redux actions for snippets

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
-import { 
-  OPEN_LIST_MODAL, 
+import {
+  OPEN_LIST_MODAL,
   CLOSE_LIST_MODAL,
   CHANGE_LIST_MODAL_NAME,
   RESET_LIST_MODAL_NAME,
   ADD_NEW_LIST,
-  SET_SELECTED_LIST
+  SET_SELECTED_LIST,
+  CHANGE_SNIPPET_NAME,
+  CHANGE_SNIPPET_SYNTAX,
+  CHANGE_SNIPPET_CODE,
+  SET_VIEW_MODE,
+  ADD_NEW_SNIPPET,
+  CHANGE_SNIPPET_LIST,
+  RESET_SNIPPET_INPUTS
 } from './types'
 
 export const openListModal = () => ({
@@ -31,4 +38,39 @@ export const addNewList = () => ({
 export const setSelectedList = (id) => ({
   type: SET_SELECTED_LIST,
   payload: id
+})
+
+//////////////////////////////////////////////////////////////////
+
+export const changeSnippetName = (value) => ({
+  type: CHANGE_SNIPPET_NAME,
+  payload: value
+})
+
+export const changeSnippetCode = (value) => ({
+  type: CHANGE_SNIPPET_CODE,
+  payload: value
+})
+
+export const setViewMode = (value) => ({
+  type: SET_VIEW_MODE,
+  payload: value
+})
+
+export const addNewSnippet = () => ({
+  type: ADD_NEW_SNIPPET
+})
+
+export const changeSnippetList = (id) => ({
+  type: CHANGE_SNIPPET_LIST,
+  payload: id
+})
+
+export const changeSnippetSyntax = (value) => ({
+  type: CHANGE_SNIPPET_SYNTAX,
+  payload: value
+})
+
+export const resetSnippetInputs = () => ({
+  type: RESET_SNIPPET_INPUTS
 })
