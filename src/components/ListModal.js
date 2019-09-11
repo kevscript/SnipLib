027ff9f4bf2b5517b29f208cc71569b3e3256c2d
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-09-11 15:57:50 +0200

    restyle modal

--- src/components/ListModal.js
+++ src/components/ListModal.js
@@ -17,7 +17,43 @@ const Container = styled.div`
 `
 
 const Modal = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background: #f4f4f4;
+  width: 600px;
+  height: 110px;
+  max-width: 90%;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  padding: 25px;
+`
+
+const ModalSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  span {
+    color: darkred;
+    font-weight: 500;
+    font-size: 14px;
+  }
+`
+
+const ButtonsContainer = styled.div``
+
+const Button = styled.button`
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+`
+
+const Input = styled.input`
+  position: relative;
+  padding: 10px;
+  font-size: 16px;
 `
 
 const ListModal = ({ lists, closeListModal, changeListModalName, addNewList, resetListModalName }) => {
@@ -39,18 +75,26 @@ const ListModal = ({ lists, closeListModal, changeListModalName, addNewList, res
   return (
     <Container>
       <Modal>
-        <label htmlFor="list-name">List Name: </label>
-        <input 
-          autoFocus={true}
-          onChange={(e) => changeListModalName(e.target.value)} 
-          onKeyUp={handleKeyUp}
-          value={nameInput}
-          type="text" 
-          name="list-name" 
-        />
-        {error ? error : null}
-        <button onClick={addNewList}>Create</button>
-        <button onClick={handleCancelation}>Cancel</button>
+        <ModalSection>
+          <Input 
+            autoFocus={true}
+            onChange={(e) => changeListModalName(e.target.value)} 
+            onKeyUp={handleKeyUp}
+            value={nameInput}
+            type="text" 
+            name="list-name" 
+            placeholder="Enter a name for your list"
+          />
+          <ButtonsContainer>
+            <Button onClick={addNewList}>Create</Button>
+            <Button onClick={handleCancelation}>Cancel</Button>
+          </ButtonsContainer>
+        </ModalSection>
+        { error && 
+            <ModalSection>
+              <span>{error}</span>
+            </ModalSection>
+        }
       </Modal>
     </Container>
   )
