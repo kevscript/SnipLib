55a8d94b1daf8f2ba85b81c82f62f53ebc6717d2
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-09-23 14:54:51 +0200

    added a Copy button for snippets in read viewMode

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
@@ -11,17 +13,57 @@ require('codemirror/mode/python/python')
 require('codemirror/mode/ruby/ruby')
 require('codemirror/mode/clike/clike')
 
+const Container = styled.div`
+  position: relative;
+`
 
-const Editor = ({ handleChange, value = 'write code here', options, lang }) => {
+const Button = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  z-index: 999;
+  cursor: pointer;
+  outline: 0;
+  border: 0;
+  background: #00adb5;
+  padding: 3px 5px;
+  color: #fff;
+  font-weight: 600;
+  
+  &:active {
+    background: #f4f4f4;
+    color: #00adb5;
+  }
+`
+
+const Editor = ({ handleChange, value = 'write code here', options, lang, snippets }) => {
+
+  const { viewMode } = snippets
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(value)
+  }
+
   return (
-    <CodeMirror
-      value={value}
-      options={{ ...options, mode: lang }}
-      onBeforeChange={(editor, data, val) => {
-        handleChange(val)
-      }}
-    />
+    <Container>
+      <CodeMirror
+        value={value}
+        options={{ ...options, mode: lang }}
+        onBeforeChange={(editor, data, val) => {
+          handleChange(val)
+        }}
+      />
+      { viewMode === 'read' && <Button onClick={handleCopy}>copy</Button> }
+    </Container>
   )
 }
 
-export default Editor
+const mapStateToProps = state => ({
+  snippets: state.snippets
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor)
