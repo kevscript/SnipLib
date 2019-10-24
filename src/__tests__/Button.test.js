a06d83f17d4b5c50f5ec4e996d7b6b33bf42dc08
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-10-24 19:54:08 +0200

    tested Button

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Button from '../components/Button'
 
-test('renders', () => {
-  const label = 'Hello'
-  const component = render(<Button label={label} />)
+describe('Button component', () => {
 
-  console.log(component)
+  it('renders with correct text and handles correct action', () => {
+    const label = 'Hello'
+    const onClick = jest.fn()
+    const { getByRole } = render(<Button label={label} handleOnClick={onClick} />)
+  
+    fireEvent.click(getByRole('button'))
+  
+    expect(onClick).toHaveBeenCalled()
+    expect(getByRole('button')).toHaveTextContent(label)
+  })
+
 })
